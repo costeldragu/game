d005937253e4781e9ef92715385a75be1ceac0f8
Dragu Costel <dragu.costel@yahoo.com>
2016-01-26 21:27:47 +0200

    Square collision detection Transition effect

--- colision_detect/js/sqare.js
+++ colision_detect/js/sqare.js
@@ -1,90 +1,105 @@
-(function ( $,window,document ){
-    "use strict";    
+(function ($, window, document) {
+    "use strict";
     var square = {
         /**
          * Rectangular coordonates
          */
         rect: {
-            top:0,
-            left:0,
-            height:0,
-            width:0,
+            top: 0,
+            left: 0,
+            height: 0,
+            width: 0
         },
-        doomElement : false,
+        doomBoard : {},
+        doomElement: false,
         //Init the element
-        init : function(options) {            
+        init: function (options) {
             //Init doom element
-            this.doomElement = $('<div>',{
-                'class':'sqare'
+            this.doomElement = $('<div>', {
+                'class': 'square'
             });
             //Set the rect if any
-            if(options.rect) {
+            if (options.rect) {
                 this.rect = options.rect;
             }
             //set sqare to board if board exits
-            if(options.board) {
-                options.board.append(this.doomElement);
+            if (options.board) {
+                options.board.dom.append(this.doomElement);
+                this.doomBoard = options.board;
+                this.startUpPosition(options.board)
             }
+
             this.render();
-            
-            
+
+
         },
-        render:function() {
-            
+        /**
+         * Start up position of rect
+         */
+        startUpPosition: function() {
+            this.rect.width = Math.floor((Math.random() * 50) + 1);
+            this.rect.height = Math.floor((Math.random() * 50) + 1);
+            this.rect.top = Math.floor((Math.random() * (this.doomBoard.height-this.rect.height)) + 1);
+            this.rect.left = Math.floor((Math.random() * (this.doomBoard.width-this.rect.width)) + 1);
+        },
+        render: function () {
             this.doomElement.css({
-                top:this.rect.top,
-                left:this.rect.left,
-                height:this.rect.height,
-                width : this.rect.width
-                
+                top: this.rect.top,
+                left: this.rect.left,
+                height: this.rect.height,
+                width: this.rect.width
             })
         }
-        
-    }
+
+    };
     //Init the 
-    $(function(){
-       //Init sqare array list
-    var sqares = new Array();
-    
-    var sqare = Object.create(square);
-    var gameWidth = $('#game_board').outerWidth()
-    var gameHeight = $('#game_board').outerHeight()
-    sqare.init({
-        rect : {
-            top:0,
-            left:10,
-            width:15,
-            height:20
-        },
-        board:$('#game_board')
-    })   
-    
-    gameLoop();
+    $(function () {
+        //Init square array list
+        var squares = [];
+        var gameWidth = $('#game_board').outerWidth();
+        var gameHeight = $('#game_board').outerHeight();
+        for(var x=0;x<10;++x) {
+            var oneSquare = Object.create(square);
+            oneSquare.init({
+                board: {
+                    dom:$('#game_board'),
+                    width:gameWidth,
+                    height:gameHeight
+                }
+            });
+            squares.push(oneSquare);
+
+        }
+
+        gameLoop();
     });
-   
+
+
+
     /**
      * Game loop
      */
     function gameLoop() {
-        
-        
+
+
         fps();
         window.requestAnimationFrame(gameLoop)
     }
-    
+
     /**
      * Calculate frame rate per seconds
-     */    
-    var frameTime = 0, lastLoop = new Date().getTime(), nextLoop = new Date().getTime()+1000;
+     */
+    var frameTime = 0, nextLoop = new Date().getTime() + 1000;
+
     function fps() {
         var thisFrameTime = new Date().getTime();
-        if(thisFrameTime<nextLoop) {
-           ++frameTime 
-        }else{
-             $('#frame_per_second').html(frameTime.toFixed(1));
-              nextLoop += 1000;
-             frameTime=0
+        if (thisFrameTime < nextLoop) {
+            ++frameTime
+        } else {
+            $('#frame_per_second').html(frameTime.toFixed(1));
+            nextLoop += 1000;
+            frameTime = 0
         }
     }
-    
-})(jQuery,window,document);
+
+})(jQuery, window, document);
---
+++ transition/js/transition.js
@@ -0,0 +1,3 @@
+/**
+ * Created by CaDyMaN on 26.01.2016.
+ */
