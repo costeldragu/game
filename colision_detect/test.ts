05894736018b1e853416f077b11a8bd6252da7c5
Dragu Costel <costel.dragu@endava.com>
2016-02-02 15:04:56 +0200

    Add ts

---
+++ colision_detect/test.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by cdragu on 02.02.2016.
+ */
+
+
+class square {
+    id: string;
+    constructor(id: string) {
+
+    };
+
+    startUpPosition() {
+
+    }
+
+    render(){
+
+    }
+
+    overlap() {
+
+    }
+}
