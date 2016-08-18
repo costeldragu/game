6a04b367bdda78a7bb168ce52f6f3e9a1ebcb024
Dragu Costel <dragu.costel@yahoo.com>
2016-08-18 23:26:09 +0300

    Signed-off-by: Dragu Costel <dragu.costel@yahoo.com>
     Changes to be committed:
    	modified:   ObjectAvoid/game.js

--- ObjectAvoid/game.js
+++ ObjectAvoid/game.js
@@ -1,5 +1,5 @@
 /**
- * Created by Dragu Costel on 27.12.2015.
+ * Created by Dragu Costel on 18.08.2016.
  */
 
 // shim layer with setTimeout fallback
@@ -15,53 +15,105 @@ window.requestAnimFrame = (function (callback) {
 
 var ball = new Object();
 
-ball.coordonates = {
+//Ball coordinates
+ball.coordinates = {
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0
+    right: 0,
+    direction: 1
 };
 
+//Ball  center coordinates based on 
+ball.center = {
+    x: 0,
+    y: 0
+};
+
+//Ball dimensions
 ball.dimensions = {
     width: 0,
     height: 0,
     radius: 0
 };
-
+//Div object
 ball.div = null;
+ball.container = null;
+ball.id = null;
 
 ball.create = function (container) {
-    var radius = Math.floor((Math.random() * 50) + 10);
+    var radius = 100;//Math.floor((Math.random() * 50) + 10);
+    this.container = container;
+    this.div = document.createElement('div');
+    this.div.setAttribute("class", "ball");
+
     this.dimensions = {
         width: radius * 2,
         height: radius * 2,
         radius: radius
     };
 
-    console.log(container.offsetHeight);
-    console.log(container.offsetWidth);
+    Object.assign(this.div.style, {
+        borderRadius: +this.dimensions.radius + "px",
+        backgroundColor: 'rgb(' + Math.floor((Math.random() * 255) + 1) + ',' + Math.floor((Math.random() * 255) + 1) + ',' + Math.floor((Math.random() * 255) + 1) + ')'
+    });
 
-    this.coordonates = {
-        top: Math.floor((Math.random() * (container.offsetHeight - radius)) + 5),
-        left: Math.floor((Math.random() * (container.offsetWidth - radius)) + 5)
+    this.randomPosition();
 
-    };
-
-    this.coordonates.right = this.coordonates.left + radius;
-    this.coordonates.bottom = this.coordonates.top + radius;
-
-    this.div = document.createElement('div');
-    this.div.setAttribute("class", "ball");
-    this.div.setAttribute("style",
-        "top:" + this.coordonates.top + "px;" +
-        "left:" + this.coordonates.left + "px;" +
-        "width:" + this.dimensions.width + "px;" +
-        "height:" + this.dimensions.height + "px;" +
-        "border-radius:" + this.dimensions.radius + "px;"
-    );
-    this.div.style.backgroundColor = 'rgb(' + Math.floor((Math.random() * 255) + 1) + ',' + Math.floor((Math.random() * 255) + 1) + ',' + Math.floor((Math.random() * 255) + 1) + ')'
 
     container.appendChild(this.div);
+
+};
+
+ball.randomPosition = function () {
+    this.coordinates = {
+        top: Math.floor((Math.random() * ( this.container.offsetHeight - this.dimensions.radius)) + 5),
+        left: Math.floor((Math.random() * ( this.container.offsetWidth - this.dimensions.radius)) + 5)
+    };
+
+    this.coordinates.right = this.coordinates.left + this.dimensions.radius;
+    this.coordinates.bottom = this.coordinates.top + this.dimensions.radius;
+    //Don't go outside of board
+    if (this.coordinates.left + this.dimensions.width >  this.container.offsetWidth) {
+        this.coordinates.left =  this.container.offsetWidth - this.dimensions.width;
+    }
+    //Don't go outside of board
+    if (this.coordinates.top + this.dimensions.height >  this.container.offsetHeight) {
+        this.coordinates.top =  this.container.offsetHeight - this.dimensions.height;
+    }
+    ball.center = {
+        x: this.coordinates.left + this.dimensions.radius,
+        y: this.coordinates.top + this.dimensions.radius
+    };
+    this.setStyle();
+};
+/**
+ * Set style
+ */
+ball.setStyle = function () {
+    Object.assign(this.div.style, {
+        top: this.coordinates.top + "px",
+        left: this.coordinates.left + "px",
+        width: this.dimensions.width + "px",
+        height: this.dimensions.height + "px"
+    });
+};
+
+ball.overlap = function (otherBall) {
+    var x = otherBall.center.x - this.center.x;
+    var y = otherBall.center.y - this.center.y;
+    var sumRad = otherBall.dimensions.radius - this.center.radius;
+    var d = Math.sqrt((x * x) + (y * y));
+
+    console.log(otherBall.center.x,this.center.x);
+    if (d < sumRad) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+ball.move = function () {
 
 };
 
@@ -80,11 +132,42 @@ document.onmousemove = function (event) {
 window.balls = [];
 function init() {
     mouseBall.create(document.getElementById('board'));
-    for (var x = 0; x < 10; ++x) {
-        var oneRound = Object.assign(ball);
+    for (var x = 0; x < 2; ++x) {
+        var oneRound = Object.assign({},ball);
         oneRound.create(document.getElementById('board'));
-        window.balls.push(oneRound);
+        oneRound.id = x;
+        //Avoid startup overlap
+
+        if(window.balls.length) {
+            window.checkOverlap = 0;
+            avoidOverLap(oneRound);
+        }
+
+        window.balls[x]=oneRound;
+
     }
+    console.log(window.balls.length);
+}
+
+function avoidOverLap(ball) {
+
+    if(window.checkOverlap > 10 ) {
+        console.log('Unable to stop overlap');
+        return true;
+    }else{
+        ++window.checkOverlap;
+    }
+
+    window.balls.forEach(function (inserted_ball) {
+console.log(ball.id , inserted_ball.id);
+        if(ball.id !=  inserted_ball.id) {
+            if(inserted_ball.overlap(ball)) {
+                ball.randomPosition();
+                avoidOverLap(ball);
+            }
+        }
+
+    });
 
 }
 
